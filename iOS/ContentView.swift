e3043dbe84a03497e8d87018cfc80cf1beeb5b18
David Hariri <david.hariri@gmail.com>
2019-09-13 13:35:02 -0400

    Initial Commit

---
+++ iOS/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  iOS
+//
+//  Created by David Hariri on 2019-09-13.
+//  Copyright © 2019 David Hariri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
